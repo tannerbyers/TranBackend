eca1968e4ca3a6cd39395a8cc066f3586f9437b1
tannerbyers <programtanner@gmail.com>
2020-04-05 00:26:23 -0400

    added cors

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const path = require('path');
 
 const app = express();
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://ancient-waters-27303.herokuapp.com/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 // Put all API endpoints under '/api'
 app.get('/*', (req, res) => {
   // Return them as json
