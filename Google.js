5203ebed0988af245e5950823c4d0cb55ac55b87
Leighton A Pitman <leightonpitman14@gmail.com>
2020-04-21 11:59:58 -0400

    add diarization: speakers are ID'd in transcripts

--- Google.js
+++ Google.js
@@ -1,10 +1,11 @@
 const { Storage } = require("@google-cloud/storage");
-const speech = require("@google-cloud/speech");
+const speech = require("@google-cloud/speech").v1p1beta1;
+const fs = require("fs");
 const storage = new Storage();
 const client = new speech.SpeechClient();
 
 async function uploadToBucket(bucketName, filename) {
-  console.log("upload to bucket requested")
+  console.log("upload to bucket requested");
   // Uploads a local file to the bucket
   await storage.bucket(bucketName).upload(filename, {
     // Support for HTTP requests made with `Accept-Encoding: gzip`
@@ -15,8 +16,8 @@ async function uploadToBucket(bucketName, filename) {
       // Enable long-lived HTTP caching headers
       // Use only if the contents of the file will never change
       // (If the contents will change, use cacheControl: 'no-cache')
-      //cacheControl: "public, max-age=31536000",
-      cacheControl: "no-cache",
+      cacheControl: "public, max-age=31536000",
+      //cacheControl: "no-cache",
     },
   });
   console.log(`${filename} uploaded to ${bucketName}.`);
@@ -51,8 +52,8 @@ async function transcribe(gcsUri) {
     encoding: "LINEAR16",
     sampleRateHertz: 16000,
     languageCode: "en-US",
-    // enableSpeakerDiarization: true,
-    // diarizationSpeakerCount: 3,
+    enableSpeakerDiarization: true,
+    // diarizationSpeakerCount: 2,
   };
 
   const audio = {
@@ -69,10 +70,32 @@ async function transcribe(gcsUri) {
   const [operation] = await client.longRunningRecognize(request);
   // Get a Promise representation of the final result of the job
   const [response] = await operation.promise();
-  const transcription = response.results
-    .map((result) => result.alternatives[0].transcript)
-    .join("\n");
-    return transcription
+  // const transcription = response.results
+  //   .map((result) => result.alternatives[0].transcript)
+  //   .join("\n");
+
+  const result = response.results[response.results.length - 1];
+  const wordsObjects = result.alternatives[0].words;
+
+  const stringifyDialog = (words) => {
+    let currSpeakerTag; // number | undefined
+    let lines = []; // Array<[number, string]>, where number is speaker tag and string is the line
+
+    for (let { speakerTag, word } of words) {
+      if (speakerTag !== currSpeakerTag) {
+        currSpeakerTag = speakerTag;
+        lines.push([speakerTag, word]);
+      } else {
+        lines[lines.length - 1][1] += ` ${word}`;
+      }
+    }
+    return lines
+      .map(([speakerTag, line]) => `Speaker${speakerTag}: ${line}`)
+      .join("\n");
+  };
+
+  console.log("TRANSCRIPT:\n", stringifyDialog(wordsObjects));
+  return stringifyDialog(wordsObjects);
 }
 
 module.exports = {
